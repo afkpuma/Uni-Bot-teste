1d3e5334d06ee0295ed6da7d237f55f7e55f5a1c
Yam Alao <afk.puma@gmail.com>
2026-02-11 07:15:42 -0300

    Primeiro commit

---
+++ app/services/kommo_service.py
@@ -0,0 +1,25 @@
+import requests
+from app.core.config import settings
+
+class KommoService:
+    def __init__(self):
+        self.headers = {
+            "Authorization": f"Bearer {settings.KOMMO_TOKEN}",
+            "Content-Type": "application/json"
+        }
+        self.base_url = settings.KOMMO_URL
+
+    def get_leads(self, query: str):
+        """Busca leads na Kommo. Se tiver query, filtra."""
+        url = f"{self.base_url}/api/v4/leads"
+        params = {}
+        
+        if query:
+            params['query'] = query
+
+        response = requests.get(url, headers=self.headers, params=params)
+        
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return {"error": f"Erro {response.status_code}", "details": response.text}
