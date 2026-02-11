7196491d5025a43fa11e1ae63ebc4181b3e28f3e
Yam Alao <afk.puma@gmail.com>
2026-02-11 20:40:02 -0300

    feat: Rota POST criar_novo_lead

--- app/controllers/lead_controller.py
+++ app/controllers/lead_controller.py
@@ -1,8 +1,24 @@
 from fastapi import APIRouter
 from app.services.kommo_service import KommoService
+from pydantic import BaseModel
+
+# Criamos um "molde" simples para garantir que o dado chegue certo
+class LeadSchema(BaseModel):
+    nome: str
+    price: int
+
+
+
 
 router = APIRouter()
 kommo_service = KommoService()
+
+
+
+@router.post("/leads")
+def criar_novo_lead(dados: LeadSchema):
+    # O FastAPI converte o JSON recebido para o objeto 'dados'
+    return kommo_service.create_lead(dados.nome, dados.price)
 
 @router.get("/leads")
 def read_leads(busca: str = None):
