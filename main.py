bfbdc3a765cf93a30b95e4350945d2d7648c99bd
Yam Alao <afk.puma@gmail.com>
2026-02-11 11:24:02 -0300

    add get_contacts and contacts_id

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from app.controllers import lead_controller
+from app.controllers import lead_controller, contact_controller
 
 app = FastAPI(title="Unicesumar Bot API")
 
-# Inclui as rotas do controlador
+# Inclui as rotas dos controladores
 app.include_router(lead_controller.router, prefix="/api")
+app.include_router(contact_controller.router, prefix="/api")
 
 @app.get("/")
 def root():
