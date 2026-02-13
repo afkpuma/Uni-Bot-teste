db65a9ed9c193c8220b81920eb754b4c91ef4226
Yam Alao <afk.puma@gmail.com>
2026-02-12 23:24:43 -0300

    new features

--- app/core/config.py
+++ app/core/config.py
@@ -4,6 +4,9 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     KOMMO_URL: str
     KOMMO_TOKEN: str
+    MONDAY_TOKEN: str
+    MONDAY_URL: str = "https://api.monday.com/v2"
+    MONDAY_BOARD_ID: int = 18399984137
 
     class Config:
         env_file = ".env"
